0d7762d2fb84652dba6d72db1362bcaddf29e760
Jim McAndrew <jim@loc8.us>
2015-08-26 15:32:17 -0600

    Finish presets compilation for iD

--- tools/compileId.js
+++ tools/compileId.js
@@ -2,7 +2,10 @@ var compileFiles = require('./compileFiles');
 var jsonschema = require('jsonschema');
 var presetSchema = require('./preset.json');
 
-var categories =  function () {
+// TODO: Pull these from this document: https://github.com/nationalparkservice/places-editor/blob/master/data/presets/schema/preset.json
+var acceptableFields = ['name','geometry','tags','addTags','removeTags','fields','icon','maki','terms','searchable','matchScore'];
+
+var categories = function () {
   compileFiles('../data/presets', function (e, rawPresets) {
     var categoryList = {};
     for (var category in rawPresets) {
@@ -12,13 +15,13 @@ var categories =  function () {
   });
 };
 
-var presets = function () {
+var presets = function (callback) {
   compileFiles('../data/presets', function (e, rawPresets) {
     var idPresets = {};
     for (var category in rawPresets) {
-      for (var subcategory in rawPresets[category]) {
-        for (var preset in rawPresets[category][subcategory]) {
-          var isValid = jsonschema.validate(rawPresets[category][subcategory][preset], presetSchema);
+      for (var subcategory in rawPresets[category].subcategories) {
+        for (var preset in rawPresets[category].subcategories[subcategory].tags) {
+          var isValid = jsonschema.validate(rawPresets[category].subcategories[subcategory].tags[preset], presetSchema);
           if (isValid.length) {
             console.error(category + '/' + subcategory + '/' + preset + ': ');
             isValid.forEach(function (error) {
@@ -30,12 +33,26 @@ var presets = function () {
             });
             process.exit(1);
           }
-          idPresets[category + '/' + subcategory + '/' + preset] = rawPresets[category][subcategory][preset];
+          var currentPreset = rawPresets[category].subcategories[subcategory].tags[preset];
+          currentPreset.searchable = currentPreset.inEditor;
+
+          for (var field in currentPreset) {
+            if (acceptableFields.indexOf(field) === -1) {
+              delete currentPreset[field];
+            }
+          }
+
+          idPresets[category + '/' + subcategory + '/' + preset] = currentPreset;
         }
       }
     }
     // console.log(JSON.stringify(idPresets, null, 2));
-    return idPresets;
+    callback(null, idPresets);
   });
 };
 
+presets(function (e, r) {
+  console.log(JSON.stringify({
+    presets: r
+  }, null, 2));
+});
