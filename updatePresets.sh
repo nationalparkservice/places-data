f9712650ca23ada7961f24cd872e4f91fe8add17
Jim McAndrew <jim@loc8.us>
2015-10-16 09:06:48 -0600

    Make preset generation easier

---
+++ updatePresets.sh
@@ -0,0 +1,17 @@
+# USAGE: updatePresets.sh PATH_TO_CSV
+# This script will run the code to generate new presets and write them to the presets file for use in iD
+# If no CSV File is specified, it will return an error
+CSV_File=$1
+
+if [ -z "$CSV_File" ];
+then
+  echo ERROR: You Must Specify a CSV file 1>&2
+  echo "USAGE: updatePresets.sh PATH_TO_CSV"
+  exit 1 # terminate and indicate error
+else
+  rm -r ./data/presets/
+  node ./tools/buildFromCSV.js $CSV_File
+  cd ./tools
+  node ./compileId.js > ../npmapPresets.js
+  cd ..
+fi
